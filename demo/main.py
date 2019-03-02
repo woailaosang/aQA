36a54d6551fbf0648fc1df5d6c1f9390c7f4418d
Zhijie Sang <2284728029@qq.com>
2019-03-02 15:57:01 +0800

    add bing search

--- demo/main.py
+++ demo/main.py
@@ -1,8 +1,37 @@
 # -*- coding:utf-8 -*-
 import web
 from web import form
-import sys
+import sys, os
 from mrc import get_an_example, any_input
+
+os.system('cd /home/sangzhijie && bash logbuptgw.sh -i 2016110858 6361311')
+
+# Import required modules.
+from azure.cognitiveservices.search.websearch import WebSearchAPI
+from azure.cognitiveservices.search.websearch.models import SafeSearch
+from msrest.authentication import CognitiveServicesCredentials
+def get_passage(question):
+    passage_list = []
+    try:
+        subscription_key = "7c06d657db2e46c4b4c1c3b65bb45fa1"
+        client = WebSearchAPI(CognitiveServicesCredentials(subscription_key))
+        web_data = client.web.search(query=question, set_lang='EN')
+        if hasattr(web_data.web_pages, 'value'):
+            data_list = [item for item in web_data.web_pages.value if item.additional_properties['language'] == 'en']
+            print("Total num: " + str(len(web_data.web_pages.value)) + '\tFiltered num: ' + str(len(data_list)))
+            for i in range(len(data_list)):
+                web_page = data_list[i]
+                name = web_page.name
+                url = web_page.url
+                snippet = web_page.snippet
+                passage_list.append([snippet, url])
+        else:
+            print("Didn't find any web pages...")
+    except:
+        os.system('cd /home/sangzhijie && bash logbuptgw.sh -i 2016110858 6361311')
+        print("get_passage: ERROR!!!")
+    return passage_list
+
 
 default_encoding = 'utf-8'
 if sys.getdefaultencoding() != default_encoding:
@@ -30,21 +59,26 @@ class mrc:
         f = mrc_form()
         get_value = web.input()
         highlight_passage, highlight_answer = "", ""
-        return render.mrc(f, highlight_passage, highlight_answer)
+        passage_list = []
+        return render.mrc(f, highlight_passage, highlight_answer, passage_list)
 
     def POST(self):
         f = mrc_form()
         post_value = web.input(query=None, passage=None)
         if hasattr(post_value, 'random'):
             f['query'].value, f['passage'].value, highlight_passage, highlight_answer = get_an_example()
-            return render.mrc(f, highlight_passage, highlight_answer)
+            passage_list = get_passage(question=f['query'].value)[:5]
+            return render.mrc(f, highlight_passage, highlight_answer, passage_list)
         query, passage = post_value.query, post_value.passage
-        f['query'].value, f['passage'].value = query, passage
+        passage_list = get_passage(question=query)[:5]
+        if passage == "":
+            passage = passage_list[0][0] if len(passage_list) > 0 else ""
         if query and passage:
             _, _, highlight_passage, highlight_answer = any_input(query, passage)
         else:
             highlight_passage, highlight_answer = "", ""
-        return render.mrc(f, highlight_passage, highlight_answer)
+        f['query'].value, f['passage'].value = query, passage
+        return render.mrc(f, highlight_passage, highlight_answer, passage_list)
 
 
 if __name__ == "__main__":
